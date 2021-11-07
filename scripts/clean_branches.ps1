5002b0cde505acce29b163a1229c481c06cd4a45
mtracewicz <m.tracewicz@gmail.com>
2021-11-07 21:10:07 +0100

    Closes #22 Added two scripts to automate common tasks with git. First creates a branch which follows our nameing convention. Second deletes all local branches except for master and current one.

---
+++ scripts/clean_branches.ps1
@@ -0,0 +1,7 @@
+$branches = (git branch -l)
+foreach ($branch in $branches) {
+    $branch_name = $branch.Trim()
+    if (!($branch_name -eq "master" -or $branch_name.StartsWith("*"))) {
+        git branch -D $branch_name
+    }
+}
---
+++ scripts/create_branch.ps1
@@ -0,0 +1,16 @@
+$issue_titles = [ordered]@{}
+$issues = (gh issue list -a mtracewicz --json "number,title" | ConvertFrom-Json)
+if ($issues.Length -eq 0) {
+    exit
+}
+
+Write-Host "Nr.`tBranch"
+foreach ($issue in $issues) {
+    $issue_number = "$($issue.number)".PadLeft(5, '0')
+    $branch = ("$($issue_number)_$($issue.title)").Replace(" ", "_").Replace(":", "")
+    $issue_titles["$($issue.number)"] = $branch
+    Write-Host "$($issue.number)`t$branch"
+}
+
+$choice = Read-Host -Prompt "Please select an issue on which You want to work"
+git checkout -b $issue_titles[$choice]
