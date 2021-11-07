bc29301de85f3b672ce04c0dc85791dcfc3a5dc0
mtracewicz <m.tracewicz@gmail.com>
2021-11-07 21:56:12 +0100

    Closes #22 Added a script to create PRs

---
+++ scripts/create_pr.ps1
@@ -0,0 +1,12 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$reviewer,
+    [Parameter(Mandatory = $true)]
+    [string]$bodyFile
+)
+
+$branchs = (git branch -l)
+$current_branch = $branchs.Where({ $_.StartsWith('*') })
+$issue_number = $current_branch.Substring(2, 5).TrimStart('0')
+
+gh pr create -d -a `@me -r $reviewer -t "Closes #$($issue_number))" -B master -F $bodyFile
