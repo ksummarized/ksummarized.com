de025d5b3c426078c29d4e23e77c0c8dae4fc7cb
Michał Tracewicz <m.tracewicz@gmail.com>
2025-03-11 21:40:54 +0100

    Closes #60 Small refactorings

--- backend/src/api/Endpoints/Tasks/DeleteTaskEndpoint.cs
+++ backend/src/api/Endpoints/Tasks/DeleteTaskEndpoint.cs
@@ -1,5 +1,7 @@
 using core.Ports;
 using Serilog;
+using api.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api.Endpoints.Tasks;
 
@@ -9,20 +11,20 @@ public static class DeleteTaskEndpoint
     public static IEndpointRouteBuilder MapDeleteTaskEndpoint(this IEndpointRouteBuilder app)
     {
         app.MapDelete(ApiEndpoints.Todo.Tasks.Delete, async (
-            ITodoService service,
-            HttpContext context,
-            int id) =>
+            [FromRoute] int Id,
+            HttpContext ctx,
+            [FromServices] ITodoService service) =>
         {
-            var userId = (Guid)context.Items["UserId"]!;
-            Log.Debug("User: {user} deleted his item: {id}", userId, id);
+            var userId = (Guid)ctx.Items["UserId"]!;
+            Log.Debug("User: {user} deleted his item: {id}", userId, Id);
             
-            var success = await service.DeleteItem(userId, id);
-            return success ? Results.Ok() : Results.BadRequest();
+            var success = await service.DeleteItem(userId, Id);
+            return success ? Results.Ok() : Results.NotFound();
         })
         .WithName(Name)
         .Produces(StatusCodes.Status200OK)
         .ProducesProblem(StatusCodes.Status400BadRequest)
-        .RequireAuthorization();
+        .RequireAuthorization(UserIdRequirement.PolicyName);
 
         return app;
     }
