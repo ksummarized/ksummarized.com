67e340fabfd8ebf2f6ad0c98f7a09fed8ddc5054
Michał Tracewicz <m.tracewicz@gmail.com>
2025-04-10 22:10:03 +0200

    Closes #60
    Fix warrnings

--- backend/src/contracts/Responses/CreateTaskResponse.cs
+++ backend/src/contracts/Responses/CreateTaskResponse.cs
@@ -4,26 +4,26 @@ public class CreateTaskResponse
 {
     public int ListId { get; set; }
     public int Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public bool Completed { get; set; }
     public DateTime? Deadline { get; set; }
-    public string Notes { get; set; }
-    public IEnumerable<Tag> Tags { get; set; }
-    public IEnumerable<Subtask> Subtasks { get; set; }
+    public string Notes { get; set; } = string.Empty;
+    public required IEnumerable<Tag> Tags { get; set; }
+    public required IEnumerable<Subtask> Subtasks { get; set; }
 
     public class Subtask
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public bool Completed { get; set; }
         public DateTime? Deadline { get; set; }
-        public string Notes { get; set; }
-        public IEnumerable<Tag> Tags { get; set; }
+        public string Notes { get; set; } = string.Empty;
+        public required IEnumerable<Tag> Tags { get; set; }
     }
 
     public class Tag
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
     }
 }
